4f531e105312e3b252c8386253a36be6df4127fa
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-07-14 11:02:23 -0700

    connected to db

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,6 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'yelp',
+  password: 'password',
+  port: 5432
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
